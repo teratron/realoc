d89af64de75d9f5edbf830f72ea41681f970bd23
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-11-09 00:34:10 +0200

    Update vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,35 @@
-import { defineConfig } from 'vite'
-// @ts-ignore
+import {defineConfig} from 'vite'
 import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //root: 'src',
-  base: pkg.homepage,
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/index.html'
-      }
+    root: 'src',
+    base: pkg.homepage,
+    appType: 'mpa',
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `$injectedColor: orange;`,
+            },
+        },
     },
-    assetsDir: 'static/css',
-    outDir: 'dist',
-    sourcemap: true,
-    minify: true,
-    cssMinify: true,
-    cssCodeSplit: false,
-  },
-  plugins: [],
+    build: {
+        rollupOptions: {
+            //input: 'src/index.ts',
+            output: {
+                entryFileNames: 'js/[name].[hash].js',
+                assetFileNames: '[ext]/[name].[hash][extname]',
+                chunkFileNames: '[name]--[hash].js',
+            }
+        },
+        //assetsDir: 'static',
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: false,
+        minify: true,
+        cssMinify: true,
+        cssCodeSplit: false,
+    },
+    publicDir: '../public',
+    plugins: [],
 })
