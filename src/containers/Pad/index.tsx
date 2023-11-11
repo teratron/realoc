7ef41669ef0a2db6596570395fc849a6a1521484
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-11-11 09:04:13 +0200

    Last time for bootstrap

---
+++ src/containers/Pad/index.tsx
@@ -0,0 +1,21 @@
+import {CSSProperties} from 'react'
+import {ContainerProps} from '../Container'
+import './_card.scss'
+
+interface CardProps extends ContainerProps {
+    className?: string
+    style?: CSSProperties
+}
+
+function Pad(props: CardProps) {
+    let className = props.className ? 'app-pad ' + props.className : 'app-pad'
+    let style = props.style ? props.style : undefined
+
+    return (
+        <div className={className} style={style}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Pad
