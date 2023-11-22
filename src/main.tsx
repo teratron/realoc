35c0e76e38cba029bd3fee1911eb84284213dd6a
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-11-23 01:22:25 +0200

    Test vercel 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter as Router} from 'react-router-dom'
 import App from './containers/App'
-
+//basename="/realoc"
 ReactDOM.createRoot(document.getElementById('app')!).render(
     <React.StrictMode>
-        <Router basename="/realoc">
+        <Router>
             <App/>
         </Router>
     </React.StrictMode>
