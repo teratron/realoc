52da897188347b2c923e2a010b70070dcf241ebb
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-11-23 12:10:23 +0200

    Updates

--- assets/js/location.D9jvHjPH.js
+++
@@ -1,1 +0,0 @@
-(e=>{if(e.search[1]==="/"){const a=e.search.slice(1).split("&").map(i=>i.replace(/~and~/g,"&")).join("?");window.history.replaceState(null,null,e.pathname.slice(0,-1)+a+e.hash)}})(window.location);
