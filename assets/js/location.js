37fc71c3f0312368d121a1a41ea392872f5f3bab
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-11-23 14:18:30 +0200

    Updates

--- assets/js/location.js
+++ assets/js/location.js
@@ -1,22 +1,12 @@
 // A crutch for problem 404 on GitHub Pages
 (loc => {
+    console.log(loc)
     if (loc.search[1] === '/') {
         let decoded = loc.search
             .slice(1)
             .split('&')
-            .map((val) => val.replace(/~and~/g, '&'))
+            .map(val => val.replace(/~and~/g, '&'))
             .join('?')
         window.history.replaceState(null, null, loc.pathname.slice(0, -1) + decoded + loc.hash)
     }
 })(window.location)
-
-// (function(l) {
-//     if (l.search[1] === '/' ) {
-//         var decoded = l.search.slice(1).split('&').map(function(s) {
-//             return s.replace(/~and~/g, '&')
-//         }).join('?');
-//         window.history.replaceState(null, null,
-//             l.pathname.slice(0, -1) + decoded + l.hash
-//         );
-//     }
-// }(window.location))
