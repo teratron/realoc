1ea6ebed0b63a9307df48c960421616f09673173
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-11-24 12:23:57 +0200

    Reorganize project structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+// A crutch for problem 404 on GitHub Pages
+import './assets/ts/location.ts'
+
 ReactDOM.createRoot(document.getElementById('app')!).render(
     <React.StrictMode>
         <App/>
